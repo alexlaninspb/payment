f8aea37c3b10523ad968cd98491484dfe1a8fa93
Alex <alexlaninspb@gmail.com>
2023-02-27 00:57:13 +0300

    fixed pom

--- src/main/java/com/example/payment/controller/PaymentController.java
+++ src/main/java/com/example/payment/controller/PaymentController.java
@@ -7,14 +7,11 @@ import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
 import java.net.URI;
@@ -33,24 +30,22 @@ public class PaymentController {
             @RequestParam(name="amount") Double amount,
             @RequestParam(name="currency") String currency,
             ModelMap model) throws IOException, JSONException, URISyntaxException {
-
-
         ResponseEntity<String> result = paymentService.process(paymentType, amount, currency);
         if (result.getStatusCode().value() == 200){
             JSONObject json = new JSONObject(result.getBody());
             JSONObject res = (JSONObject) json.get("result");
-            String url = (String) res.get("redirectUrl");
-            URI uri = new URI(url);
-            HttpHeaders httpHeaders =
-                    new HttpHeaders();
-            httpHeaders.setLocation(uri);
-            return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
+            try{
+                String url = (String) res.get("redirectUrl");
+                URI uri = new URI(url);
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.setLocation(uri);
+                return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+                return new ResponseEntity<>(result.getBody(), result.getStatusCode());
+            }
         } else {
-            //URI uri = new URI("pay");
-            //HttpHeaders httpHeaders = new HttpHeaders();
-            //httpHeaders.setLocation(uri);
             return new ResponseEntity<>(result.getBody(), result.getStatusCode());
         }
     }
-
 }
